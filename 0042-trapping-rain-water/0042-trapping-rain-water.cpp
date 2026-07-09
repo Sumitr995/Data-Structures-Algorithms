ffd53d3179cc2c529b531d4dc8284e4d1bbeac9a
Sumit Rathod <sumitrathod-extc@atharvacoe.ac.in>
2026-07-09 23:08:21 +0530

    Time: 4 ms (9.38%), Space: 27.4 MB (6.93%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int n = height.size();
+        vector<int> lmax(n);
+        vector<int> rmax(n);
+
+
+        // first element has no left side
+        lmax[0] = height[0];
+
+        // last element has no right side
+        rmax[n-1] = height[n-1];
+
+        // calculate max wall from left
+        for(int i=1; i<n; i++){
+            lmax[i] = max(lmax[i-1],height[i]);
+        }
+
+
+        // calculate max wall from right
+        for(int i=n-2; i>=0; i--){
+            rmax[i] = max(rmax[i+1],height[i]);
+        }
+
+
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            ans += min(lmax[i],rmax[i]) - height[i];
+        }
+
+        return ans;
+    }
+};
