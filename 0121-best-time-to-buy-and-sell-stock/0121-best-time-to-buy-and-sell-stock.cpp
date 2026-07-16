736cdc5bf0a6ddc3dc9d08f152c33976be848bb5
Sumit Rathod <sumitrathod-extc@atharvacoe.ac.in>
2026-07-16 17:57:12 +0530

    Time: 0 ms (100%), Space: 97.3 MB (60.92%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int MaxProfit = 0; 
-        int BestBuy = prices[0];
+        int n = prices.size();  
+        int MaxProfit = 0;  // highest Profit
+        int BestBuy = prices[0]; // Prev Best Buying time of current price
 
         for(int i = 0; i<n; i++){
-            if(prices[i] > BestBuy){
+            if(prices[i] > BestBuy){ // checking 
                 MaxProfit = max(MaxProfit, prices[i] - BestBuy);
             }
             BestBuy = min(BestBuy, prices[i]);
