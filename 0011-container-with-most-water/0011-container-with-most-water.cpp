17a55fa0d878a3ddc9f7a6232e13f1796dc7c170
Sumit Rathod <sumitrathod-extc@atharvacoe.ac.in>
2026-07-21 21:48:30 +0530

    Time: 0 ms (100%), Space: 62.9 MB (79.18%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int maxArea(vector<int>& heights) {
-
-        int maxArea = 0;
-
+        int MaxArea = 0;
         int lp = 0;
-        int rp = heights.size() - 1;
+        int rp = heights.size() - 1; 
 
-        while(lp < rp){
+        while (lp < rp) {
+            int width = rp - lp;
+            int ht = min(heights[lp], heights[rp]);
+            int currArea = width * ht;
 
-            int width = rp - lp; // distance between pointers = width
-            int ht = min(heights[lp], heights[rp]); // smallest height between pointers
-            int currArea = width * ht; // area of square
+            MaxArea = max(currArea, MaxArea);
 
-            maxArea = max(maxArea, currArea); // compare and push max area
-
-            // move smaller height pointer
-            if(heights[lp] < heights[rp]){
+            if (heights[lp] < heights[rp]) {
                 lp++;
-            }
-            else{
-                rp--;
+            } else {
+                rp--; 
             }
         }
-
-        return maxArea;
+        return MaxArea;
     }
 };
